2600425282249b605eeb2e96493d6acf2fd0f1ae
user <sami.elnakadi@gmail.com>
2019-04-10 10:55:10 +0200

    checkbox

--- db.php
+++ db.php
@@ -10,11 +10,9 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-if (isset ($_POST['ville']) AND ($_POST['haut']) AND ($_POST['bas'])){
-    $ville=$_POST['ville'];
-    $haut=$_POST['haut'];
-    $bas=$_POST['bas'];
-    $bdd->exec("INSERT INTO Météo(ville,haut,bas) VALUES('$ville','$haut','$bas')");
+if (isset ($_POST['tache'])){
+    $tache=$_POST['tache'];
+    $bdd->exec("INSERT INTO Todolist(Tâche) VALUES('$tache')");
 };
 
 
@@ -22,9 +20,21 @@ if (isset ($_POST['ville']) AND ($_POST['haut']) AND ($_POST['bas'])){
 
 if(isset($_POST['supprimer']) AND isset($_POST['delete'])){
     foreach($_POST['delete'] as $select){
-    $bdd->exec("DELETE FROM Météo WHERE ville='$select'");
-    echo "test1";
+    $bdd->exec("DELETE FROM Todolist WHERE tâche='$select'");
+    $bdd->exec("INSERT INTO Archive(Archive) VALUES('$select')");
     }
 }
 header('Location: index.php');
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+    <link type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link type="text/css" rel="stylesheet" href="/assets/db.css">
+    <title>To do list</title>
+</head>
--- index.php
+++ index.php
@@ -10,49 +10,70 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-$resultat = $bdd -> query('SELECT * FROM Météo');
-$donnees = $resultat->fetch();
+$resultat = $bdd -> query('SELECT * FROM Todolist');
+$resultatArchive = $bdd -> query('SELECT * FROM Archive');
 
 ?>
 
 <!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
+    <link type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link type="text/css" rel="stylesheet" href="./assets/db.css">
+    <title>To do list</title>
+</head>
 
-<body>
-    <section class="mainForm">
-        <div class="container form">   
-                <form class="col-md-12" role="form" name="inscription" method="post" action="test.php" id='form'>
+<body>  
+        <div class="container">
+        <h1> Activités de la journée</h1>
+            <div class="row">     
+                <div class="col-md-6">
+                
+                    <form role="form" name="inscription" method="post" action="db.php" id='form'>
+                    <div class="list">
+                        <?php
+                        echo '<table><th><h3>A faire</h3></th>';
+                        while ($donnees = $resultat->fetch())
+                        {
+                        echo '<tr>';
+                        echo'<td><input type="checkbox" name="delete[]" id="delete" value="'.$donnees['Tâche'].'">'.$donnees['Tâche'].'</td>';
+                        echo '</tr>';
+                        }
+                        echo '</table>';
+                        $resultat->closeCursor();
+                        ?>                    
+                    </div>        
+                </div>
+                <div class="col-md-6">
                     <?php
-                    echo '<table><th>Ville</th>';
-                    echo '<th>Haut</th>';
-                    echo '<th>Bas</th>';
-                    while ($donnees = $resultat->fetch())
+
+                    echo '<div class="archive"><table><th><h3>Archive</h3></th>';
+                    while ($donneesArchive = $resultatArchive->fetch())
                     {
                       echo '<tr>';
-                      echo'<td><input type="checkbox" name="delete[]" id="delete" value="'.$donnees['ville'].'">'.$donnees['ville'].'</td>';
-                      echo '<td>'.$donnees['haut'].'</td>';
-                      echo '<td>'.$donnees['bas'].'</td>';
+                      echo'<td>'.$donneesArchive['Archive'].'</td>';
                       echo '</tr>';
                     }
-                    echo '</table>';
-                    $resultat->closeCursor();
-                    ?>                    
-                    
+                    echo '</table></div>';
+                    $resultatArchive->closeCursor();
+                    ?>
+                </div>
+            </div>
+
                     <div class = "row">
                         <div class="all col-md-6">  
-                            <label for="ville" >Ville:</label>
-                            <input class="form-control" type="text" maxlength="30" placeholder="Votre ville" name="ville"/>
-
-                            <label for="haut">Température haute:</label>
-                            <input class="form-control" type="text" maxlength="30"  placeholder="Température haute" name="haut"/>
-
-                            <label for="basse">Température basse:</label>
-                            <input class="form-control" type="text" maxlength="30"  placeholder="Température basse" name="bas"/>
+                            <label for="tache" >Ajouter une tâche</label>
+                            <input class="form-control" type="text" maxlength="60" placeholder="Tâche à faire" name="tache"/>
                         </div>
                     </div>
 
-                    <button type="submit" class="btn btn-primary" name="valider" value="Envoyer">Envoyer</button>
+                    <button type="submit" class="btn btn-primary" name="valider" value="Envoyer">Enregistrer</button>
                     <button type="submit" class="btn btn-primary" name="supprimer" value="supprimer">Supprimer</button>
             </form>
         </div>
-    </section>   
+     
 </body>
